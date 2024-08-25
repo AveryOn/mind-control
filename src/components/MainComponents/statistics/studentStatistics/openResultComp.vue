cdc5bb1eb5aeee26d1f4ef682c420052ff695b8e
AveryOn <lnkonion2@gmail.com>
2024-08-26 00:47:41 +0300

    dev

--- src/components/MainComponents/statistics/studentStatistics/openResultComp.vue
+++ src/components/MainComponents/statistics/studentStatistics/openResultComp.vue
@@ -54,13 +54,62 @@
             </div>
 
             <!-- Список вопросов и ответов -->
-            <div class="w-full flex flex-column align-items-center">
+            <div class="w-full flex flex-column align-items-center gap-3 mt-3">
                 <div 
-                class="question-item px-3 py-2" 
+                class="question-item px-3 py-2 flex flex-column" 
+                :class="(computeStateAnswer(question.id).answer?.isCorrect)? 'success' : 'failed'"
                 v-for="question in store.openResultStudent?.questions" 
                 :key="question.id"
                 >
-                    {{ question.question }}
+                    <Badge class="no" :value="question.number" size="small"></Badge>
+
+                    <!-- Текст Вопроса -->
+                    <span class="ml-3 mb-1" style="font-size: 1rem">{{ question.question }}</span>
+
+                    <!-- Подпись о количестве возможных вариантов ответа для данного вопроса -->
+                    <span v-if="question.type !== 'text'" class="light-text ml-auto mx-auto mb-2">{{ `( ${computeSignTypeAnswer(question.type)} )` }}</span>
+
+                    <!-- Checkbox Варианты ответа -->
+                    <span 
+                    class="checkbox-variant"
+                    v-if="question.type === 'checkbox'" 
+                    v-for="variant in question.checkboxAnswers"
+                    >{{ variant.answer }}</span>
+
+                    <!-- Radio Варианты ответа -->
+                    <span 
+                    class="checkbox-variant"
+                    v-if="question.type === 'radio'" 
+                    v-for="variant in question.radioAnswers"
+                    >{{ variant.answer }}</span>
+
+                    <!-- Ответ на вопрос -->
+                    <div class="w-full flex flex-column mt-2 mx-3">
+                        <span class="light-text mb-2">Ваш ответ:</span>
+
+                        <!-- Checkbox ОТВЕТ -->
+                        <div class="flex gap-1 flex-wrap" v-if="question.type === 'checkbox'" >
+                            <Tag 
+                            class="text-xs"
+                            v-for="answer in JSON.parse(computeStateAnswer(question.id).answer?.answer!)"
+                            :key="answer"
+                            :value="answer"
+                            ></Tag>
+                        </div>
+
+                        <!-- Обычный ОТВЕТ -->
+                        <span v-else class="answer-text w-max px-2 ml-2">
+                            {{ computeStateAnswer(question.id).answer?.answer }}
+                        </span>
+                    </div>
+
+                    <!-- Значение ВЕРНО / НЕВЕРНО -->
+                    <span 
+                    class="state-question mx-auto w-full text-center"
+                    :class="(computeStateAnswer(question.id).answer?.isCorrect)? 'success' : 'failed'"
+                    >
+                        {{ computeStateAnswer(question.id).isCorrect? 'Верно' : 'Не верно' }}
+                    </span>
                 </div>
             </div>
         </div>
@@ -69,6 +118,7 @@
 
 <script setup lang="ts">
 import { useMainStore } from '@/stores/mainStore';
+import type { QuestionTypes } from '@/types/testTypes';
 import { computeMinutesByMs, formattedDateByTemplate, fromNow } from '@/utils/timeUtils';
 import { computed, defineProps } from 'vue';
 
@@ -79,15 +129,53 @@ const props = defineProps<{
 
 // Вычисление класса состояния результат (success | failed)
 const computeClasses = computed(() => {
-    const classList = { success: false, failed: false };
-    if(store.openResultStudent?.isSuccess === true) {
-        classList.success = true;
+    try {
+        const classList = { success: false, failed: false };
+        if(store.openResultStudent?.isSuccess === true) {
+            classList.success = true;
+        }
+        else {
+            classList.failed = true;
+        }
+        return classList;
+    } catch (err) {
+        console.error('/src/components/MainComponents/statistics/studentStatistics/openResultComp.vue: computed[computeClasses] => ', err);
+        throw err;
     }
-    else {
-        classList.failed = true;
+
+});
+
+// Вычисление того верный ли ответ на вопрос или нет
+const computeStateAnswer = computed(() => {
+    return (questionId: number | undefined) => {
+        try {
+            if (questionId) {
+                const findedAnswer = store.openResultStudent?.answers.find((answer) => {
+                    if (answer.questionId === questionId) return true;
+                });
+                return  { isCorrect: findedAnswer?.isCorrect, answer: findedAnswer };
+            } 
+            else return { isCorrect: false, answer: null };
+        } catch (err) {
+            console.error('/src/components/MainComponents/statistics/studentStatistics/openResultComp.vue: computed[computeStateAnswer] => ', err);
+            throw err;
+        }
     }
-    return classList;
-})
+});
+
+// Вычислить тип ответа (несколько ответов или один ответ) зависит от типа вопроса
+const computeSignTypeAnswer = computed(() => {
+    return (type: QuestionTypes) => {
+        try {
+            if(type === 'checkbox') return 'Несколько вариантов ответа';
+            else if(type === 'radio') return 'Один вариант ответа';
+            else return '';
+        } catch (err) {
+            console.error('/src/components/MainComponents/statistics/studentStatistics/openResultComp.vue: computed[computeSignTypeAnswer] => ', err);
+            throw err;
+        }
+    }
+});
 
 </script>
 
@@ -115,9 +203,52 @@ const computeClasses = computed(() => {
     user-select: none;
 }
 .question-item {
-    width: 300px;
+    position: relative;
+    width: 500px;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
     border-radius: 3px;
     font-size: 0.9rem;
+}
+.question-item.success {
+    border-top: 4px solid var(--success-color);
+}
+.question-item.failed {
+    border-top: 4px solid var(--required-color);
+}
+.no {
+    position: absolute;
+    left: 1px;
+    top: 1px;
+}
+.checkbox-variant {
+    width: max-content;
+    padding: 0.05rem 0.5rem;
+    background-color: rgba(212, 212, 212, 0.383);
+    border-radius: 3px;
+    margin-left: 1.3rem;
+    user-select: none;
+    cursor: default;
+    border: 1px dotted var(--basic-border-color);
+}
+.checkbox-variant + .checkbox-variant {
+    margin-top: 0.2rem;
+}
+
+.answer-text {
+    background-color: rgba(212, 212, 212, 0.383);
+    border-bottom: 2px solid var(--basic-border-color);
+    border-top-right-radius: 2px;
+    border-top-left-radius: 2px;
+}
+
+.state-question {
+    border-radius: 4px;
+    margin-top: 1rem;
+}
+.state-question.success {
+    background-color: var(--result-item-hover-bg-success);
+}
+.state-question.failed {
+    background-color: var(--result-item-hover-bg-failed);
 }
 </style>
